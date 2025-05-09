afe986bd97aecd5bb29cba1947144921eca27d83
Andres <andres.calderon-r@mail.escuelaing.edu.co>
2025-05-08 23:55:24 -0500

    GlobalExceptionHandler

---
+++ src/main/java/edu/eci/cvds/ECIBienestarGym/exceptions/GYMException.java
@@ -0,0 +1,17 @@
+package edu.eci.cvds.ECIBienestarGym.exceptions;
+
+public class GYMException extends Exception {
+    public static final String USERT_NOT_FOUND = "User Not Found";
+    public static  final String ROUTINE_NOT_FOUND = "Routine Not Found";
+    public static final String RESERVE_NOT_FOUND = "Reserve Not Found";
+    public static final String REPORT_NOT_FOUND = "Report Not Found";
+    public static final String PHYSICAL_PROGRESS_NOT_FOUND = "Physical progress Not Found";
+    public static final String GYM_SESION_NOT_FOUND = "Gym Session Not Found";
+    public GYMException(String message) {
+        super(message);
+    }
+    @Override
+    public String toString() {
+        return getMessage();
+    }
+}
