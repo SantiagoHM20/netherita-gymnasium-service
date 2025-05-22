b85bcdcf9cf8bcc36be44b715e94be716950794d
Andres <andres.calderon-r@mail.escuelaing.edu.co>
2025-05-22 00:21:35 -0500

    Third try to add the statistic endpoints to swagger

--- src/main/java/edu/eci/cvds/ECIBienestarGym/controller/StatisticsController.java
+++ src/main/java/edu/eci/cvds/ECIBienestarGym/controller/StatisticsController.java
@@ -38,6 +38,7 @@ public class StatisticsController {
 
     @GetMapping("/session-statistics")
     @Operation(summary = "Obtener estadísticas de sesiones por entrenador")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'ENTRENADOR')")
     public ResponseEntity<ApiResponse<Map<String, Object>>> getSessionStatisticsByTrainer() {
         List<GymSession> sessions = gymSessionService.getAllGymSessions();
 
@@ -56,6 +57,7 @@ public class StatisticsController {
 
     @GetMapping("/students-by-session")
     @Operation(summary = "Cantidad de estudiantes por sesión")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'ENTRENADOR')")
     public ResponseEntity<ApiResponse<Map<String, Integer>>> getStudentsBySession() {
         List<Reservation> reservations = reservationService.getAllReservations();
 
@@ -69,9 +71,10 @@ public class StatisticsController {
     }
     @GetMapping("/physical-progress/user/{userId}")
     @Operation(summary = "Obtener estadísticas de progreso físico por usuario")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'ENTRENADOR','ESTUDIANTE')")
     public ResponseEntity<ApiResponse<List<PhysicalProgressDTO>>> getPhysicalProgressByUser(@PathVariable String userId) {
         User user = new User();
-        user.setId(userId); // solo necesitas el ID para la búsqueda
+        user.setId(userId);
 
         List<PhysicalProgress> progressList = physicalProgressService.getPhysicalProgressByUserId(user);
 
