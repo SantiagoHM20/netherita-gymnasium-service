edace0723e998da4f114489fd55d902f5b4a0f60
Andres <andres.calderon-r@mail.escuelaing.edu.co>
2025-05-21 22:45:56 -0500

    Intento 2 de añadir en el swagger el controlador de estadisticas

--- src/main/java/edu/eci/cvds/ECIBienestarGym/controller/StatisticsController.java
+++ src/main/java/edu/eci/cvds/ECIBienestarGym/controller/StatisticsController.java
@@ -21,7 +21,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("api/stats")
+@RequestMapping("/api")
 @Tag(name = "Statistics", description = "Controladores relacionados para las estadisticas")
 public class StatisticsController {
     private final GymSessionService gymSessionService;
